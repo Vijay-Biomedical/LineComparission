b5ccf1be4cdef84f648d428ff2dff1a3fe1dd317
Thiru <vijayofficial580@gmail.com>
2022-04-02 13:57:57 +0530

    uc2

--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ namespace Linecommparision
     {
         static void Main(string[] args)
         {
-            double x1, x2, y1, y2;
-            x1 = 12d;
-            x2 = 13d;
-            y1 = 11d;
-            y2 = 10d;
+            double x1, x2, y1, y2, p1, p2, q1, q2;
+            x1 = 12d;x2 = 13d;y1 = 11d;y2 = 10d;
+            p1 = 15;p2 = 18;q1 = 13;q2 = 15;
             var distance_l1= Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            Console.WriteLine("distance:" + distance_l1);
-
-            
-           
+            var distance_l2 = Math.Sqrt((Math.Pow(p1 - p2, 2) + Math.Pow(q1 - q2, 2)));
+            if (distance_l1 == distance_l2)
+                Console.WriteLine("Two lines are equal ");
+            else
+                Console.WriteLine("Not equal");
+       
         }
     }
 }
